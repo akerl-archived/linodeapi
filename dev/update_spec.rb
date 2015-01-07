dfbce7d184c0b7ee13a45f387467930c0a679879
Les Aker <me@lesaker.org>
2015-01-06 19:05:20 -0500

    update spec script to be ordered

--- dev/update_spec.rb
+++ dev/update_spec.rb
@@ -3,6 +3,7 @@
 require 'httparty'
 require 'pp'
 require 'erb'
+require 'active_support/ordered_hash'
 
 SPEC_PATH = 'lib/linodeapi/spec.rb'
 
@@ -15,37 +16,41 @@ module LinodeAPI
 <%= clean %>
 end
 '
+# rubocop:enable all
 
 raw = HTTParty.get('https://api.linode.com/?api_action=api.spec').body
 raw = JSON.parse(raw)['DATA']['METHODS']
 
-spec = raw.reduce(Hash.new) do |acc, (method, info)|
+spec = ActiveSupport::OrderedHash.new
+
+raw.sort_by(&:first).each_with_object(spec) do |(method, info), acc|
   groups = method.split('.').map(&:to_sym)
   name = groups.pop
 
-  params = info['PARAMETERS'].map do |k, v|
+  params = info['PARAMETERS'].sort_by(&:first).map do |k, v|
+    # rubocop:disable Style/MultilineOperationIndentation
     [
       k.downcase.to_sym,
-      {
+      ActiveSupport::OrderedHash[
         desc: v['DESCRIPTION'],
         type: v['TYPE'].to_sym,
-        required: v['REQUIRED'],
-      }
+        required: v['REQUIRED']
+      ]
     ]
+    # rubocop:enable Style/MultilineOperationIndentation
   end
 
   local = groups.reduce(acc) do |layout, new|
     layout[new] ||= { type: :group, subs: {} }
     layout[new][:subs]
   end
+
   local[name] = {
     type: :call,
     desc: info['DESCRIPTION'],
     throws: info['THROWS'].split(','),
-    params: Hash[params],
+    params: ActiveSupport::OrderedHash[params]
   }
-
-  acc
 end
 
 clean = ''
